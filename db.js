6aa12e0af5b18974f1fc4f2072ccffa0498580fc
enes-sertkan <enessertkan451@gmail.com>
2023-10-13 20:45:29 -0400

    changed a lot of files

--- db.js
+++ db.js
@@ -1,21 +1,17 @@
-// db.js
+// const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
+// const dbUrl = 'mongodb+srv://enessertkan:Password123@cluster0.55mfjdg.mongodb.net/?retryWrites=true&w=majority';
 
-// MongoDB connection URL (replace with your actual MongoDB URL)
-const dbUrl = 'mongodb://http://localhost:27017/comp3095-mongodb'; // Replace with your MongoDB URL
+// mongoose.connect(dbUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// // Check if the connection is successful
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//     console.log('Connected to MongoDB');
+// });
 
-// Check if the connection is successful
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-module.exports = mongoose;
-
+// module.exports = mongoose;
--- routes/employee.js
+++ routes/employee.js
@@ -1,91 +1,69 @@
 const express = require('express');
 const router = express.Router();
-const Employee = require('../routes/employee'); // Assuming you have an Employee model
+const Employee = require('../models/employeeModel');
 
-// POST /api/v1/emp/employees
-router.post('/', async (req, res) => {
+// Route to create a new employee
+router.post('/employees', async (req, res) => {
     try {
-        // Create a new employee using the request body
-        const newEmployee = new Employee(req.body);
-        await newEmployee.save();
 
-        // Return a response with a 201 status code on success
-        res.status(201).json(newEmployee);
+        const newEmployee = new Employee(req.body);
+        const savedEmployee = await newEmployee.save();
+        res.status(201).json(savedEmployee);
+  
     } catch (error) {
-        // Handle any errors and return an error response
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(400).json({ error: 'Failed to create an employee.' });
     }
 });
 
-// GET /api/v1/emp/employees
-router.get('/', async (req, res) => {
+// Route to get all employees
+router.get('/employees', async (req, res) => {
     try {
-        // Fetch all employees from the MongoDB collection
         const employees = await Employee.find();
-
-        // Return the list as a JSON response with a 200 status code
         res.status(200).json(employees);
     } catch (error) {
-        // Handle any errors and return an error response
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(400).json({ error: 'Failed to fetch employees.' });
     }
 });
 
-// GET /api/v1/emp/employees/{eid}
-router.get('/:eid', async (req, res) => {
+// Route to get an employee by ID
+router.get('/employees/:eid', async (req, res) => {
+    const { eid } = req.params;
     try {
-        const employeeId = req.params.eid;
-
-        // Fetch a specific employee by employee ID from the MongoDB collection
-        const employee = await Employee.findById(employeeId);
-
+        const employee = await Employee.findById(eid);
         if (!employee) {
-            // If employee is not found, return a 404 response
-            res.status(404).json({ error: 'Employee not found' });
-        } else {
-            // Return the employee data as a JSON response with a 200 status code if found
-            res.status(200).json(employee);
+            return res.status(404).json({ error: 'Employee not found.' });
         }
+        res.status(200).json(employee);
     } catch (error) {
-        // Handle any errors and return an error response
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(400).json({ error: 'Failed to fetch the employee.' });
     }
 });
 
-// PUT /api/v1/emp/employees/{eid}
-router.put('/:eid', async (req, res) => {
+// Route to update an employee by ID
+router.put('/employees/:eid', async (req, res) => {
+    const { eid } = req.params;
     try {
-        const employeeId = req.params.eid;
-
-        // Update the employee data based on the request body
-        const updatedEmployee = await Employee.findByIdAndUpdate(employeeId, req.body, { new: true });
-
+        const updatedEmployee = await Employee.findByIdAndUpdate(eid, req.body, { new: true });
         if (!updatedEmployee) {
-            // If employee is not found, return a 404 response
-            res.status(404).json({ error: 'Employee not found' });
-        } else {
-            // Return a response with a 200 status code on success
-            res.status(200).json(updatedEmployee);
+            return res.status(404).json({ error: 'Employee not found.' });
         }
+        res.status(200).json(updatedEmployee);
     } catch (error) {
-        // Handle any errors and return an error response
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(400).json({ error: 'Failed to update the employee.' });
     }
 });
 
-// DELETE /api/v1/emp/employees?eid=xxx
-router.delete('/', async (req, res) => {
+// Route to delete an employee by ID
+router.delete('/employees/:eid', async (req, res) => {
+    const { eid } = req.params;
     try {
-        const employeeId = req.query.eid;
-
-        // Delete a specific employee by employee ID from the MongoDB collection
-        await Employee.findByIdAndRemove(employeeId);
-
-        // Return a response with a 204 status code on success
+        const deletedEmployee = await Employee.findByIdAndRemove(eid);
+        if (!deletedEmployee) {
+            return res.status(404).json({ error: 'Employee not found.' });
+        }
         res.status(204).send();
     } catch (error) {
-        // Handle any errors and return an error response
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(400).json({ error: 'Failed to delete the employee.' });
     }
 });
 
