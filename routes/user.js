9516d2228e4a34df5f13b09fb71af6f204ea72ce
enes-sertkan <enessertkan451@gmail.com>
2023-10-15 22:14:29 -0400

    commit to submit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel'); // Import the User model
+const User = require('../models/userModel');
 
 // Route to create a new user
 router.post('/signup', async (req, res) => {
