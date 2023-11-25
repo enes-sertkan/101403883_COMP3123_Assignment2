2f4aac52ded25ef74e1655e1190e9ef950b9b378
enes-sertkan <enessertkan451@gmail.com>
2023-11-24 21:04:40 -0500

    restarting project

--- 101403883_comp3123_assignment2_reactjs/src/App.js
+++ 101403883_comp3123_assignment2_reactjs/src/App.js
@@ -1,38 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import './style.css';
-
-
-// Import your components here
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import DashboardPage from './pages/DashboardPage';
-import AddEmployeePage from './pages/AddEmployeePage';
-import EditEmployeePage from './pages/EditEmployeePage';
-import ViewEmployeePage from './pages/ViewEmployeePage';
-import DeleteEmployeePage from './pages/DeleteEmployeePage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/add-employee" element={<AddEmployeePage />} />
-          <Route path="/edit-employee/:id" element={<EditEmployeePage />} />
-          <Route path="/view-employee/:id" element={<ViewEmployeePage />} />
-          <Route path="/delete-employee/:id" element={<DeleteEmployeePage />} />
-          {/* You can add more routes here as your application grows */}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
