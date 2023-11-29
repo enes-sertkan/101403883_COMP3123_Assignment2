605437f5f47bfe036be874e8ce9fb5eabe11355b
enes-sertkan <enessertkan451@gmail.com>
2023-11-29 17:13:32 -0500

    better structure for the assignment

---
+++ front-end/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import LoginPage from './pages/Login';
+import SignupPage from './pages/SignupPage';
+import Dashboard from './pages/Dashboard';
+import AddEmployee from './pages/AddEmployee';
+import EditEmployee from './pages/EditEmployee';
+import EmployeeDetails from './pages/EmployeeDetails';
+import Employee from './pages/Employee'
+import Home from './pages/Home'
+import PrivateRoute from './pages/PrivateRoute.jsx'
+import './pages/style.css'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginPage />}></Route>
+      <Route path='/signup' element={<SignupPage />}></Route>
+      <Route path='/employee_detail/:id' element={<EmployeeDetails />}></Route>
+      <Route path='/dashboard' element={
+        <PrivateRoute >
+          <Dashboard />
+        </PrivateRoute>
+      }>
+        <Route path='' element={<Home />}></Route>
+        <Route path='/dashboard/employee' element={<Employee />}></Route>
+        <Route path='/dashboard/add_employee' element={<AddEmployee />}></Route>
+        <Route path='/dashboard/edit_employee/:id' element={<EditEmployee />}></Route>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
