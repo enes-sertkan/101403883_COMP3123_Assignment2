933a9913fc03d94e1c52360c631fc4bed0e2f145
enes-sertkan <enessertkan451@gmail.com>
2023-11-29 20:57:13 -0500

    retrieving the codes

---
+++ front-end/src/pages/Dashboard.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import axios from "axios";
+
+const Dashboard = () => {
+  const anvigate = useNavigate()
+  axios.defaults.withCredentials = true
+  const handleLogout = () => {
+    axios.get('http://localhost:5000/api/v1/user/dashboard')
+    .then(result => {
+      if(result.data.Status) { 
+        localStorage.removeItem("valid")
+        anvigate('/')
+      }
+    })
+  }
+
+
+  return (
+    <div className="dashboard-container">
+      <nav className="sidebar bg-dark">
+        <Link to="/dashboard" className="brand d-flex align-items-center text-white text-decoration-none">
+          <span className="fs-5 fw-bolder">Employee Manager</span>
+        </Link>
+        <ul className="nav-list">
+          <li>
+            <Link to="/dashboard" className="nav-item">
+              <i className="bi-speedometer2"></i>
+              <span>Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="api/v1/user/dashboard/employee" className="nav-item">
+              <i className="bi-people"></i>
+              <span>Manage Employees</span>
+            </Link>
+          </li>
+          <li onClick={handleLogout}>
+            <a className="nav-item">
+              <i className="bi-power"></i>
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="content-area">
+        <div className="top-bar">
+          <h4>Employee Management System</h4>
+        </div>
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+export default Dashboard;
---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import './style.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+    const [values, setValues] = useState({
+        username: '', // Changed from email to username
+        password: ''
+    });
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/v1/user/login', values);
+            if (response.data.status) {
+                localStorage.setItem("valid", true);
+                navigate('/dashboard');
+            } else {
+                setError(response.data.message);
+            }
+        } catch (err) {
+            console.error(err);
+            setError(err.response?.data?.message || 'An error occurred during login');
+        }
+    };
+
+    return (
+        <div className="loginPage">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="submit">Login</button>
+                {error && <p>{error}</p>}
+            </form>
+        </div>
+    );
+};
+
+
+export default Login
